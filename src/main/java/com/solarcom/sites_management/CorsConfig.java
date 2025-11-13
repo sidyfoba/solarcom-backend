16bee5e93cd51e50cdc3c7bcebd2a01ac753380c
Sidy FOBA <sidyfoba.pro@gmail.com>
2025-11-13 02:36:11 +0000

    cors & args

--- src/main/java/com/solarcom/sites_management/CorsConfig.java
+++ src/main/java/com/solarcom/sites_management/CorsConfig.java
@@ -1,22 +1,37 @@
 package com.solarcom.sites_management;
 
-import org.springframework.context.annotation.Configuration;
+import java.util.Arrays;
 
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 
 
 @Configuration
-public class CorsConfig implements WebMvcConfigurer {
+public class CorsConfig {
+@Value("${app.cors.allowed-origins:http://localhost:3000}")
+private String allowedOrigins;
+//	@Override
+//	public void addCorsMappings(CorsRegistry registry) {
+//		registry.addMapping("/**").allowedOriginPatterns("http://192.168.1.29:5173","http://192.168.1.26:5173", "https://codeps.sn")
+//				.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS").allowedHeaders("*");
+////				.exposedHeaders("Authorization").allowCredentials(true);
+//	}
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                .allowedOrigins("http://localhost:5173")  // no trailing slash
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .exposedHeaders("Authorization")
-                .allowCredentials(true);
-    }
+	@Bean
+	public CorsFilter corsFilter() {
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowCredentials(true);
+		config.setAllowedOrigins(Arrays.asList(allowedOrigins.split(",")));
+		config.addAllowedHeader("*");
+		config.addAllowedMethod("*");
+
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		source.registerCorsConfiguration("/**", config);
+		return new CorsFilter(source);
+	}
 }
